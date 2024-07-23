9663134f26d4bad52da443cd5fb26d092b5a71e2
Alexander AS. Santa Ortega <1036926835@andesbpo.local>
2024-07-23 14:52:43 -0500

    Modal de encabezado y boton

---
+++ front/src/componentes/BotonBlanco.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../estilos/BotonBlanco.css"
+
+export const BotonBlanco = () => {
+  return (
+    <div>
+        <button className='botonBlanco'>Descargar BD</button>
+    </div>
+  )
+}
