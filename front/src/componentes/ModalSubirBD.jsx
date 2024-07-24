e0122b8ebf7507efbe06efcd1ee897ee0b262e7e
Alexander AS. Santa Ortega <1036926835@andesbpo.local>
2024-07-24 16:49:13 -0500

    asdawd

--- front/src/componentes/ModalSubirBD.jsx
+++ front/src/componentes/ModalSubirBD.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from 'react'
 import "../estilos/ModalSubirBD.css"
+import { BotonVerde } from '../componentes/BotonVerde.jsx'
 
 export const ModalSubirBD = ({modalAbierto}) => {
   
     if(!modalAbierto) return null
 
     return (
-    <div className='contenedorSubirBD'>
-        <div className='modalSubirBD'>
-            <div>
-                <h2>Subir Base de Datos</h2>
-                <button>Cerrar modal</button>
-            </div>
+    <div>
+        <header className='titulo'>
+           <h2>Subir Base de Datos</h2> 
+        </header>
+        
+            <div className='modalSubirBD'>
+                <div className='contenedorBotones'>
+                    <div>
+                    <BotonVerde className='Boton' texto={'Base de Datos 1'}/>
+                    </div>
+                    <div>
+                    <BotonVerde texto={'Base de Datos 2'}/>
+                    </div>
+                    <div>
+                    <BotonVerde texto={'Base de Datos 3'}/>
+                    </div>
+                </div>
         </div>
     </div>
   )
