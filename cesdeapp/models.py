1da095be9e0953caa73259541e5565ba0016ae96
JeffersonMontoya <chechemontoya1997@gmail.com>
2024-07-25 12:19:07 -0500

    cambios backend

--- cesdeapp/models.py
+++ cesdeapp/models.py
@@ -22,6 +22,9 @@ class Estados(models.Model):
 class Programa(models.Model):
     nombre = models.CharField(max_length=40)
     descripcion = models.TextField(max_length=300)
+    
+    def __str__(self):
+        return self.nombre
 
 
 class Aspirantes(models.Model):
