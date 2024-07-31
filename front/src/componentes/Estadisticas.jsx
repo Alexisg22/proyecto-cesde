f7181eb2d35b1954812120cc5eb329257fd8859d
Sebastian SD. Duque Salazar <1193485552@andesbpo.local>
2024-07-31 08:13:28 -0500

    cambios en estadisticas

--- front/src/componentes/Estadisticas.jsx
+++ front/src/componentes/Estadisticas.jsx
@@ -14,17 +14,17 @@ export const Estadisticas = () => {
             </div>
             <div className='contenido'>
             
-                <Estadistica id='contactabilidad' label='Contactabilidad' dato='contactabilidad'/>
-                <Estadistica id='noContactabilidad' label='No contactabilidad' dato='noContactabilidad'/>
-                <Estadistica id='porcentajeConversion' label='Porcentaje de conversion' dato='porcentajeConversion'/>
-                <Estadistica id='cantidadMatriculas' label='Cantidad de matriculas' dato='cantidadMatriculas'/>
-                <Estadistica id='cantidadLiquidaciones' label='Cantidad de liquidaciones' dato='cantidadLiquidaciones'/>
-                <Estadistica id='enGestion' label='En gestion' dato='enGestion'/>
-                <Estadistica id='sinGestion' label='Sin gestion' dato='sinGestion'/>
-                <Estadistica id='matriculado' label='Matriculado' dato='matriculado'/>
-                <Estadistica id='liquidacion' label='Liquidacion' dato='liquidacion'/>
-                <Estadistica id='cancelado' label='Cancelados' dato='cancelado'/>
-                <Estadistica id='noGestionable' label='No gestionable' dato='noGestionable'/>
+                <Estadistica id='contactabilidad' label='Contactabilidad' dato='20'/>
+                <Estadistica id='noContactabilidad' label='No contactabilidad' dato='10'/>
+                <Estadistica id='porcentajeConversion' label='Porcentaje de conversion' dato='25'/>
+                <Estadistica id='cantidadMatriculas' label='Cantidad de matriculas' dato='50'/>
+                <Estadistica id='cantidadLiquidaciones' label='Cantidad de liquidaciones' dato='100'/>
+                <Estadistica id='enGestion' label='En gestion' dato='150'/>
+                <Estadistica id='sinGestion' label='Sin gestion' dato='30'/>
+                <Estadistica id='matriculado' label='Matriculado' dato='90'/>
+                <Estadistica id='liquidacion' label='Liquidacion' dato='11'/>
+                <Estadistica id='cancelado' label='Cancelados' dato='5'/>
+                <Estadistica id='noGestionable' label='No gestionable' dato='16'/>
                
             </div>
 
