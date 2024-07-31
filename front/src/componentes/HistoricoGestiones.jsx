6948dc1a7e838cf4c9f27198b3b9acaafef27676
Sebastian SD. Duque Salazar <1193485552@andesbpo.local>
2024-07-31 08:54:51 -0500

    arreglo de modal historico

--- front/src/componentes/HistoricoGestiones.jsx
+++ front/src/componentes/HistoricoGestiones.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import '../estilos/HistoricoGestiones.css'
 
-export const HistoricoGestiones = ({cerrarModal, modalAbiertoHistorico}) => {
+export const HistoricoGestiones = ({ cerrarModal, modalAbiertoHistorico }) => {
 
-    if(!modalAbiertoHistorico) return 
+    if (!modalAbiertoHistorico) return
 
     const columnas = [
         { id: 'fechaGestion', etiqueta: 'Fecha Gestión' },
         { id: 'asesor', etiqueta: 'Asesor' },
         { id: 'descripcion', etiqueta: 'Descripcion' },
         { id: 'resultadoGestion', etiqueta: 'Resultado Gestión' },
-      ];
-    
-      // Datos de ejemplo (reemplazar con tus datos reales)
-      const datos = [
+    ];
+
+    // Datos de ejemplo (reemplazar con tus datos reales)
+    const datos = [
         {
             fechaGestion: '3162840984',
             asesor: '34567890',
@@ -25,81 +25,81 @@ export const HistoricoGestiones = ({cerrarModal, modalAbiertoHistorico}) => {
             asesor: '34567890',
             descripcion: 'Sofía Gómez',
             resultadoGestion: '3',
-        },{
+        }, {
             fechaGestion: '3162840984',
             asesor: '34567890',
             descripcion: 'Sofía Gómez',
             resultadoGestion: '3',
-        },{
+        }, {
             fechaGestion: '3162840984',
             asesor: '34567890',
             descripcion: 'Sofía Gómez',
             resultadoGestion: '3',
-        },{
+        }, {
             fechaGestion: '3162840984',
             asesor: '34567890',
             descripcion: 'Sofía Gómez',
             resultadoGestion: '3',
-        },{
+        }, {
             fechaGestion: '3162840984',
             asesor: '34567890',
             descripcion: 'Sofía Gómez',
             resultadoGestion: '3',
         },
-      ];
-      
-  return (
+    ];
 
-    <div className='cotenedorModal'>
-        <div className='modalHistoricoGestiones'>
-            <div className="tituloModalHistorico">
-                <h2>Historial de gestiones del aspirante</h2>
-                <button onClick={cerrarModal}>X</button>
+    return (
+
+        <div className='cotenedorModal'>
+            <div className='modalHistoricoGestiones'>
+                <div className="tituloModalHistorico">
+                    <h2>Historial de gestiones del aspirante</h2>
+                    <button onClick={cerrarModal}>X</button>
+                </div>
+
+                <div className='datosAspiranteHistorico' >
+                    <p><strong>Aspirante: </strong>Alexander Santa Ortega</p>
+                    <p><strong>Celular: </strong>3044233452</p>
+                    <p><strong>Programa: </strong>Tecnico en desarrollo de software</p>
+                    <p><strong>Sede: </strong>Rionegro</p>
+                </div>
+
+                <hr />
+                <main className="tablaHistorico" id="tablaClientesHistorico">
+                    <section className="cuerpoTablaHistorico">
+                        <table className='tablaHistorico'>
+                            <thead className='cabezaTablaHistorico'>
+                                <tr>
+                                    {columnas.map(columna => [columna.id] && (
+                                        <th key={columna.id} id={columna.id}>
+                                            {columna.etiqueta}
+                                        </th>
+                                    )
+                                    )}
+                                </tr>
+                            </thead>
+                            <tbody className='cuerpoTablaHistorico'>
+                                {datos.map((row, index) => (
+                                    <tr className='filaTablaAspirantesHistorico'>
+                                        {columnas.map(columna => [columna.id] && (
+                                            <td key={columna.id}>
+                                                {columna.id === 'estadoAspirante' ? (
+                                                    <p className={row[columna.id].toLowerCase()}>{row[columna.id]}</p>
+                                                ) : (
+                                                    row[columna.id]
+                                                )}
+                                            </td>
+                                        )
+                                        )}
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </section>
+
+                </main>
+
             </div>
-
-            <div className='datosAspiranteHistorico' >
-                <p><strong>Aspirante: </strong>Alexander Santa Ortega</p>
-                <p><strong>Celular: </strong>3044233452</p>
-                <p><strong>Programa: </strong>Tecnico en desarrollo de software</p>
-                <p><strong>Sede: </strong>Rionegro</p>
-            </div>
-
-            <hr />
-            <main className="tablaHistorico" id="tablaClientesHistorico">     
-                <section className="cuerpoTablaHistorico">
-                    <table className='tablaHistorico'>
-                    <thead className='cabezaTablaHistorico'>
-                        <tr>
-                        {columnas.map(columna => [columna.id] && (
-                            <th key={columna.id} id={columna.id}>
-                                {columna.etiqueta}
-                            </th>
-                            )
-                        )}
-                        </tr>
-                    </thead>
-                    <tbody className='cuerpoTablaHistorico'>
-                        {datos.map((row, index) => (
-                        <tr className='filaTablaAspirantesHistorico'>
-                            {columnas.map(columna => [columna.id] && (
-                                <td key={columna.id}>
-                                {columna.id === 'estadoAspirante' ? (
-                                    <p className={row[columna.id].toLowerCase()}>{row[columna.id]}</p>
-                                ) : (
-                                    row[columna.id]
-                                )}
-                                </td>
-                            )
-                            )}
-                        </tr>
-                        ))}
-                    </tbody>
-                    </table>
-                </section>
-      
-    </main>
-
         </div>
-    </div>
-  )
+    )
 }
