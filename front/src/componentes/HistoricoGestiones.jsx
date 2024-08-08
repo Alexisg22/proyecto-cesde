e0982531fcee39a8a80818010bf8577121401788
JulianG <juliangiral12@gmail.com>
2024-08-08 15:33:28 -0500

    se trabaja en historico gesiones

--- front/src/componentes/HistoricoGestiones.jsx
+++ front/src/componentes/HistoricoGestiones.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-import '../estilos/HistoricoGestiones.css'
+import '../estilos/HistoricoGestiones.css';
+import { obtenerHistoricoAspirante } from '../api/aspirantes.api';
+import { useState, useEffect } from 'react';
 
 export const HistoricoGestiones = ({ cerrarModal, modalAbiertoHistorico }) => {
-
+    
     if (!modalAbiertoHistorico) return
+
+    const [historicoGestiones, setHistoricoGestiones] = useState()
+
+
+    useEffect(() => {
+        async function cargarHistoricoGestiones() {
+            const numeroCelular = "3044174055";
+            const respuesta = await obtenerHistoricoAspirante(numeroCelular);
+            const historicoGestiones = respuesta.data;
+    
+            // Combinar todos los arrays internos en uno solo
+            const mapeado = historicoGestiones.flatMap(historico => 
+                historico.fechas_gestiones_por_aspirante.map((gestion) => ({
+                    nombre: gestion.nombre_completo,
+                    celular: gestion.celular,
+                    fecha: gestion.fecha,
+                    asesor: gestion.asesor,
+                    descripcion: gestion.descripcion,
+                    tipoGestion: gestion.tipo_gestion,
+                    resultadoGestion: gestion.resultado_gestion, // Asegúrate de que el nombre sea correcto
+                    programa: gestion.programa,
+                    sede: gestion.sede,
+                }))
+            );
+    
+            setHistoricoGestiones(mapeado);
+        }
+        cargarHistoricoGestiones();
+    }, []);
+
+    console.log(historicoGestiones)
+    
 
     const columnas = [
         { id: 'fechaGestion', etiqueta: 'Fecha Gestión' },
@@ -65,11 +98,19 @@ export const HistoricoGestiones = ({ cerrarModal, modalAbiertoHistorico }) => {
                 </div>
 
                 <div className='datosAspiranteHistorico' >
-                    <p><strong>Aspirante: </strong>Alexander Santa Ortega</p>
-                    <p><strong>Celular: </strong>3044233452</p>
-                    <p><strong>Programa: </strong>Tecnico en desarrollo de software</p>
-                    <p><strong>Sede: </strong>Rionegro</p>
-                </div>
+
+                    {historicoGestiones ? (
+                    <p>no se encontraron datos</p>
+                    ) : (
+                        <>
+                            <p><strong>Aspirante: </strong>{historicoGestiones[0].nombre}</p>
+                            <p><strong>Celular: </strong>{historicoGestiones[0].celular}</p>
+                            <p><strong>Programa: </strong>{historicoGestiones[0].programa}</p>
+                            <p><strong>Sede: </strong>{historicoGestiones[0].sede}</p>
+                        </>
+                    )}
+                    
+                </div> 
 
                 <hr />
                 <main className="tablaHistorico" id="tablaClientesHistorico">
