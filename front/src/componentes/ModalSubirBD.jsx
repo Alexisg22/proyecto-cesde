6cdb6220aa3e54d3c9ee477f717a675b1528fe14
Sebastian SD. Duque Salazar <1193485552@andesbpo.local>
2024-08-09 08:01:56 -0500

    modificacion del modal subir BD

--- front/src/componentes/ModalSubirBD.jsx
+++ front/src/componentes/ModalSubirBD.jsx
@@ -4,31 +4,39 @@ import axios from 'axios';
 import "../estilos/ModalSubirBD.css";
 import "../estilos/BotonVerde.css";
 
+// Componente ModalSubirBD
 export const ModalSubirBD = ({ cerrarModal, modalAbierto }) => {
-    
-    const { register, 
-            handleSubmit,
-            formState: {errors}, 
-        } = useForm();
+
+    // useForm para manejar el formulario y la validación
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+ 
+
+    // useState para manejar el estado de los nombres de los archivos subidos
     const [nombreArchivo1, setNombreArchivo1] = useState('');
     const [nombreArchivo2, setNombreArchivo2] = useState('');
     const [nombreArchivo3, setNombreArchivo3] = useState('');
     const [nombreArchivo4, setNombreArchivo4] = useState('');
 
+    // Función para capturar el nombre del archivo seleccionado en cada input
     const capturarNombreInput = (e) => {
-        const archivo = e.target.files[0];
-        const idInput = e.target.id;
+        const archivo = e.target.files[0]; // Obtiene el archivo seleccionado
+        const idInput = e.target.id; // Obtiene el ID del input que disparó el evento
 
-        if (!archivo) return;
+        if (!archivo) return; // Si no hay archivo seleccionado, retorna
 
+        // Actualiza el estado correspondiente al input que disparó el evento
         if (idInput === 'BD1') setNombreArchivo1(archivo.name);
         if (idInput === 'BD2') setNombreArchivo2(archivo.name);
         if (idInput === 'BD3') setNombreArchivo3(archivo.name);
         if (idInput === 'BD4') setNombreArchivo4(archivo.name);
     }
-    
+
+    // Función para manejar el envío del formulario
     const enviarBD = handleSubmit(async (data) => {
+        console.log(data); // Muestra los datos del formulario en la consola
         
+        // Crea un objeto FormData para enviar los archivos
         const formData = new FormData();
         formData.append('predictivo', data.predictivo ? data.predictivo[0] : null);
         formData.append('matricula', data.matricula ? data.matricula[0] : null);
@@ -36,73 +44,108 @@ export const ModalSubirBD = ({ cerrarModal, modalAbierto }) => {
         formData.append('SMS', data.SMS ? data.SMS[0] : null);
 
         try {
+            // Envío de la petición POST al servidor usando Axios
             const response = await axios.post('http://localhost:8000/cesde/cargar_csv/', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
             });
-            console.log('Enviado', response.data);
+            console.log('Enviado', response.data); // Muestra la respuesta del servidor en la consola
+           
+            window.location.reload(); // Recarga la página después de que se haya enviado con éxito
         } catch (error) {
+            // Captura y muestra errores en caso de que falle la petición
             console.error('Error al enviar los datos:', error);
         }
     });
-    
 
+    // Si el modal no está abierto, no se muestra nada
     if (!modalAbierto) return null;
 
     return (
         <div className='contenedorSubirBD'>
             <div className='modalSubirBD'>
-
                 <div className='botonCerrar'>
                     <button className='cerrarModal' onClick={cerrarModal}>X</button>
                 </div>
 
                 <h1 className='tituloSubirBD'>Subir Base de Datos</h1>
 
+                {/* Formulario para subir archivos */}
                 <form className='contenedorBotonesBD' encType='multipart/form-data' onSubmit={enviarBD}>
+                    
+                    {/* Input para subir la BD Predictivo */}
                     <div className='contenedorInpuSubirBD'>
-                        <input onChange={capturarNombreInput} type="file" id='BD1' name='predictivo' className='inputSubirBD'
-                            {...register('predictivo')}
+                        <input  type="file" id='BD1' name='predictivo' className='inputSubirBD'
+                            {...register('predictivo', {onChange: capturarNombreInput}, {
+                                required: {
+                                    value: true,
+                                    message: 'esta BD es requerida'
+                                }
+                            })}
                         />
+                        {/* Muestra el mensaje de error si no se cumple la validación */}
+                        {errors.predictivo && <samp>{errors.predictivo.message}</samp>}
                         <label htmlFor="BD1" className='labelSubirBD'>
                             BD Predictivo
                         </label>
+                        {/* Muestra el nombre del archivo seleccionado */}
                         {nombreArchivo1 && <p className='nombreArchivo'>{nombreArchivo1}</p>}
                     </div>
 
+                    {/* Input para subir la BD Matriculas */}
                     <div className='contenedorInpuSubirBD'>
-                        <input onChange={capturarNombreInput} type="file" id='BD2' name='matricula' className='inputSubirBD'
-                            {...register('matricula')}
+                        <input  type="file" id='BD2' name='matricula' className='inputSubirBD'
+                            {...register('matricula',{onChange: capturarNombreInput}, {
+                                required: {
+                                    value: true,
+                                    message: 'esta BD es requerida'
+                                }
+                            })}
                         />
+                        {errors.matricula && <samp>{errors.matricula.message}</samp>}
                         <label htmlFor="BD2" className='labelSubirBD'>
                             BD Matriculas
                         </label>
                         {nombreArchivo2 && <p className='nombreArchivo'>{nombreArchivo2}</p>}
                     </div>
 
+                    {/* Input para subir la BD Whatsapp */}
                     <div className='contenedorInpuSubirBD'>
-                        <input onChange={capturarNombreInput} type="file" id='BD3' name='whatsapp' className='inputSubirBD'
-                            {...register('whatsapp')}
+                        <input  type="file" id='BD3' name='whatsapp' className='inputSubirBD'
+                            {...register('whatsapp',{onChange: capturarNombreInput}, {
+                                required: {
+                                    value: true,
+                                    message: 'esta BD es requerida'
+                                }
+                            })}
                         />
+                        {errors.whatsapp && <samp>{errors.whatsapp.message}</samp>}
                         <label htmlFor="BD3" className='labelSubirBD'>
                             BD Whatsapp
                         </label>
                         {nombreArchivo3 && <p className='nombreArchivo'>{nombreArchivo3}</p>}
                     </div>
 
+                    {/* Input para subir la BD SMS */}
                     <div className='contenedorInpuSubirBD'>
-                        <input onChange={capturarNombreInput} type="file" id='BD4' name='SMS' className='inputSubirBD'
-                            {...register('SMS')}
+                        <input  type="file" id='BD4' name='SMS' className='inputSubirBD'
+                            {...register('SMS',{onChange: capturarNombreInput}, {
+                                required: {
+                                    value: true,
+                                    message: 'esta BD es requerida'
+                                }
+                            })}
                         />
+                        {errors.SMS && <samp>{errors.SMS.message}</samp>}
                         <label htmlFor="BD4" className='labelSubirBD'>
                             BD SMS
                         </label>
                         {nombreArchivo4 && <p className='nombreArchivo'>{nombreArchivo4}</p>}
                     </div>
 
+                    {/* Botón para enviar el formulario */}
                     <button id='baseDatos4' className='inputEnviarFormulario' type='submit'>Enviar</button>
-                    
                 </form>
             </div>
         </div>
