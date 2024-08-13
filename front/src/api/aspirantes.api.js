667fe29c315e976b38c57a3cf3e10a63a25a6046
Sebastian SD. Duque Salazar <1193485552@andesbpo.local>
2024-08-13 08:06:38 -0500

    subir cambios

--- front/src/api/aspirantes.api.js
+++ front/src/api/aspirantes.api.js
@@ -27,9 +27,7 @@ export const obtenerTodosAspirantesConFiltros = (objetoFiltros) => {
   return  axios.get(`http://127.0.0.1:8000/cesde/aspirantes-filter/?cantidad_llamadas=${objetoFiltros.cantidadLlamadas}&cantidad_whatsapp=${objetoFiltros.cantidadWhatsapps}&cantidad_gestiones=${objetoFiltros.cantidadGestiones}&dias_ultima_gestion=${objetoFiltros.diasUltimaGestion}&fecha_ultima_gestion=${objetoFiltros.fechaUltimaGestion}&tipificacion_ultima_gestion=${objetoFiltros.tipificacionUltimaGestion}&estado_aspirante=${objetoFiltros.estadoAspirante}&programa=${objetoFiltros.programaFormacion}&sede=${objetoFiltros.sede}&nit_empresa=${objetoFiltros.nitEmpresa}`)
 }
 
-export const obtenerEstadisticasGenerales = () =>{
-  return axios.get('http://127.0.0.1:8000/cesde/estadisticas/');
-}
+
 
 
 
